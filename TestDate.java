b604ebd4b106f4c61b27357c9010e904bb52645a
godeline <godelineboy@gmail.com>
2021-12-05 21:16:57 +0100

    challenge datesjava02

---
+++ TestDate.java
@@ -0,0 +1,28 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+class TestDate {
+
+    public static void main(String[] args) {
+
+        //Scanner sc = new Scanner(System.in);
+        //System.out.println("Please fill a day (1-31) :");
+        //int day = sc.nextInt();
+        //System.out.println("Please fill a month (1-12) :");
+        //int month = sc.nextInt();
+        //System.out.println("Please fill a year :");
+        //int year = sc.nextInt();
+        //sc.close();
+
+        // your code after this line
+
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyy");
+        Calendar c = Calendar.getInstance();
+        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
+        Date d = c.getTime();
+        System.out.println("The date is : " + sdf.format(d));
+        System.out.println("The day of week is : " + dayOfWeek);
+    }
+}
