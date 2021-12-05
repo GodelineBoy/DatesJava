aa57efc8c9c988b3120e78aebe6ae8fead30fcde
godeline <godelineboy@gmail.com>
2021-12-05 22:10:23 +0100

    rectif code

--- TestDate.java
+++ TestDate.java
@@ -1,28 +1,30 @@
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
 
 class TestDate {
 
     public static void main(String[] args) {
 
-        //Scanner sc = new Scanner(System.in);
-        //System.out.println("Please fill a day (1-31) :");
-        //int day = sc.nextInt();
-        //System.out.println("Please fill a month (1-12) :");
-        //int month = sc.nextInt();
-        //System.out.println("Please fill a year :");
-        //int year = sc.nextInt();
-        //sc.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please fill a day (1-31) :");
+        int day = sc.nextInt();
+        System.out.println("Please fill a month (1-12) :");
+        int month = sc.nextInt();
+        System.out.println("Please fill a year :");
+        int year = sc.nextInt();
+        sc.close();
 
         // your code after this line
 
         SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyy");
-        Calendar c = Calendar.getInstance();
-        int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-        Date d = c.getTime();
-        System.out.println("The date is : " + sdf.format(d));
-        System.out.println("The day of week is : " + dayOfWeek);
+        GregorianCalendar calendar = new GregorianCalendar(year, month, day);
+        Date date = calendar.getTime();
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+            System.out.println("The date is : " + sdf.format(date));
+            System.out.println("The day of week is : " + dayOfWeek);
     }
 }
